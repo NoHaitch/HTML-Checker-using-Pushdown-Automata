9023a7612ced5c1fa2fcb01f17b913f1cd06c9b5
Bagas <bagassambega@gmail.com>
2023-11-25 15:11:48 +0700

    fix: main updated

--- src/main.py
+++ src/main.py
@@ -1,11 +1,105 @@
 import os
+import argparse
+import sys
 
-# initialize base of PDA
-start_input = ""
 
-# global variables
-# id
-# class
-# style
+# Fungsi untuk parse HTML ke array
+def parseHTMLfile(filename):
+    contentofFile = []
+    file = open(filename, "r")
+    isComment = False
+    dash = False
+    secondDash = False
+    for line in file:
+        syntax = ""
+        startSpace = True
+        for i in range(len(line)):
+            if startSpace:
+                if line[i] == " ":
+                    continue
+                else:
+                    startSpace = False
+            if line[i] == '<' and line[i + 1] == '!':
+                isComment = True
+            if isComment:
+                if line[i] == '-':
+                    dash = True
+                if dash and line[i] == '-':
+                    secondDash = True
+                    dash = False
+                if secondDash and line[i] == '>':
+                    isComment = False
+                    secondDash = False
+                continue
+            else:
+                if line[i] == '\n':
+                    continue
+                syntax += line[i]
+        syntax.removeprefix(" ")
+        syntax = syntax.replace("-->", "")
+        syntax.removesuffix(" ")
+        syntax.removesuffix("\n")
+        if syntax != '' and len(syntax) != 0 and syntax != '\n' and syntax != '\t' and syntax != '\r' and syntax != '\0':
+            doubleSpace = True
+            while doubleSpace:
+                if "  " in syntax:
+                    syntax = syntax.replace("  ", " ")
+                else:
+                    doubleSpace = False
+            removeSpaceCloseTag = True
+            while removeSpaceCloseTag:
+                if " >" in syntax:
+                    syntax = syntax.replace(" >", ">")
+                elif "> " in syntax:
+                    syntax = syntax.replace("> ", ">")
+                elif " <" in syntax:
+                    syntax = syntax.replace(" <", "<")
+                else:
+                    removeSpaceCloseTag = False
+            contentofFile.append(syntax.strip())
+    return contentofFile
 
-print("Hello World!")
+
+if __name__ == "__main__":
+    # Inisialisasi untuk mengambil argumen PDA.txt dan juga file HTML
+    parser = argparse.ArgumentParser(description="Masukkan PDA dan HTML File!")
+    parser.add_argument("PDAFile", nargs="?", default=None, help="Masukkan nama file PDA")
+    parser.add_argument("HTMLFile", nargs="?", default=None, help="Masukkan nama file HTML")
+    args = parser.parse_args()
+
+    # Cek apakah file PDA dan HTML ada, dan jika ada di-parse dan di-save di HTML array
+    if not os.path.exists(args.PDAFile) or args.PDAFile is None:
+        print("File PDA tidak ditemukan!")
+        sys.exit()
+    else:
+        if not os.path.exists(args.HTMLFile) or args.HTMLFile is None:
+            print("File HTML tidak ditemukan!")
+            sys.exit()
+        else:
+            htmlArray = parseHTMLfile(args.HTMLFile)
+
+    # Inisialisasi PDA dan string input dari HTML
+    PDAFile = open(args.PDAFile, "r")
+    PDA = []
+    for line in PDAFile:
+        if "#" in line:
+            continue
+        else:
+            PDA.append(line.split())
+    delta = [" " for i in range(5)]
+    string = ""
+    for i in range(len(htmlArray)):
+        string += htmlArray[i]
+    print(string)
+
+    # Mulai parsing string HTML
+    html = False
+    head = False
+    body = False
+    checker = 0
+    command = ""
+
+
+
+
+
